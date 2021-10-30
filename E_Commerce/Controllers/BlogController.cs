942b6c8a2bc929f5b1cb68d212fabf0ab1bef3f4
Dương Phạm <pdtduong03@gmail.com>
2021-10-30 10:43:00 +0700

    handle Layout Blog

--- E_Commerce/Controllers/BlogController.cs
+++ E_Commerce/Controllers/BlogController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blogs.Helpers;
+using E_Commerce.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PagedList.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +12,53 @@ namespace E_Commerce.Controllers
 {
     public class BlogController : Controller
     {
-        public IActionResult Index()
+        private readonly MarketDBContext _context;
+       
+        public BlogController(MarketDBContext context)
         {
-            return View();
+            _context = context;
+        }
+        [Route("blog.html", Name="Blog")]
+        public IActionResult Index(int? page)
+        {
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            var pageSize = Utilities.PAGE_SIZE;
+            var lsPpsts = _context.Posts
+                .AsNoTracking()
+                .Include(x => x.Account)
+                .OrderBy(x => x.PostId);
+
+            PagedList<Post> models = new PagedList<Post>(lsPpsts, pageNumber, pageSize);
+            ViewBag.CurrentPage = pageNumber;
+            return View(models);
+        }
+
+        // GET: Admin/AdminPosts/Details/5
+        [Route("/tin-tuc/{Alias}-{id}.html", Name = "BlogDetails")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts
+                .AsNoTracking()
+                .Include(p => p.Account)
+                .SingleOrDefaultAsync(m => m.PostId == id);
+            var ls = _context.Posts
+                .AsNoTracking()
+                .Where(x => x.Published == true && x.PostId != id)
+                .Take(3)
+                .OrderBy(x => x.CreateDate)
+                .ToList();
+
+            if (post == null)
+            {
+                return RedirectToAction("Index");
+            }
+            ViewBag.BaiVietLienQuan = ls;
+            return View(post);
         }
     }
 }
