5184139d14835dffe3fa01cb32b727cff92ebae7
Dương Phạm <pdtduong03@gmail.com>
2021-11-01 14:57:57 +0700

    hanlde_layout_Home_Product

---
+++ E_Commerce/ModelViews/ProductHomeVM.cs
@@ -0,0 +1,14 @@
+﻿using E_Commerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Commerce.ModelViews
+{
+    public class ProductHomeVM
+    {
+        public Category category { get; set; }
+        public List<Product> lsProducts { get; set; }
+    }
+}
