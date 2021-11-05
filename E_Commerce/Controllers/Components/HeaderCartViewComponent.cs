78137081897de859cbd755c9b77a0c36a425b8db
Dương Phạm <pdtduong03@gmail.com>
2021-11-05 16:59:39 +0700

    cart

---
+++ E_Commerce/Controllers/Components/HeaderCartViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using E_Commerce.Extension;
+using E_Commerce.ModelViews;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Controllers.Components
+{
+    public class HeaderCartViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var cart = HttpContext.Session.Get<List<CartItem>>("CART");
+            return View(cart);
+        }
+    }
+}
